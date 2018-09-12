9889c9b34e6bbffc7651b45f603849ac5127bf58
Huy <huy@gohuygo.com>
2018-09-11 18:51:49 -0700

    Finish ajax logic to vote and server side logic to update/create vote records

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -2,11 +2,23 @@ class BillboardsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @billboards = Billboard.all.sort_by(&:score)
+    @billboards = Billboard.all.sort_by(&:score).reverse
   end
 
   def update
-    puts "HELLO! BILLBOARDS#UPDATE"
-    render :json => {'hello': 'world'}
+    billboard_id = params[:id]
+    direction    = params[:direction]
+
+    current_vote = current_user.votes.find_by_billboard_id(billboard_id)
+
+    if current_vote.present?
+      current_vote.update_attribute(:direction, direction)
+    else
+      current_user.votes.create(billboard_id: billboard_id, direction: direction)
+    end
+
+    new_score = Billboard.find_by_id(billboard_id).score
+
+    render :json => {billboard_id: billboard_id, score: new_score}
   end
 end
