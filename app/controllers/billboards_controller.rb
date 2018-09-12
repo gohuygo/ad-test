75aad3a77b3ac33fda55fc9fc6cefa30b80ad9a0
Huy <huy@gohuygo.com>
2018-09-11 18:15:06 -0700

    Sort billboard by score and basic edit view

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -2,7 +2,6 @@ class BillboardsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @billboards = Billboard.all
-    puts "HELLO!", current_user.inspect
+    @billboards = Billboard.all.sort_by(&:score)
   end
 end
--- app/models/billboard.rb
+++ app/models/billboard.rb
@@ -1,12 +1,8 @@
 class Billboard < ActiveRecord::Base
   has_many :votes
 
-  def self.ordered_by_votes
-
-  end
-
   def score
     return 0 if votes.blank?
-    votes.map(&:direction_value).sum
+    @score ||= votes.map(&:direction_value).sum
   end
 end
