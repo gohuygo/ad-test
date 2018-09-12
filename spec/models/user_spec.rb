3b820a347e1fae5c0b15dff4b4f32c096d9cc2bf
Huy <huy@gohuygo.com>
2018-09-11 20:24:50 -0700

    Add specs for upvote_billboard? and downvote_billboard?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,79 @@ require 'rails_helper'
 
 RSpec.describe 'User' do
   describe '#upvoted_billboard?' do
-    # TODO
+    let(:user)      { FactoryBot.create(:user)      }
+    let(:billboard) { FactoryBot.create(:billboard) }
+
+    context 'when user has not voted' do
+      it 'returns false' do
+        expect(user.upvoted_billboard?(billboard.id)).to eq false
+      end
+    end
+
+    context 'when user has voted' do
+      let!(:vote) do
+        FactoryBot.create(
+          :vote,
+          user_id: user.id,
+          billboard_id: billboard.id,
+          direction: direction
+        )
+      end
+
+      context 'when upvoted' do
+        let(:direction) { 'up'}
+
+        it 'returns true' do
+          expect(user.upvoted_billboard?(billboard.id)).to eq true
+        end
+      end
+
+      context 'when downvoted' do
+        let(:direction) { 'down'}
+
+        it 'returns false' do
+          expect(user.upvoted_billboard?(billboard.id)).to eq false
+        end
+      end
+    end
   end
 
   describe '#downvoted_billboard?' do
-    # TODO
+    let(:user)      { FactoryBot.create(:user)      }
+    let(:billboard) { FactoryBot.create(:billboard) }
+
+    context 'when user has not voted' do
+      it 'returns false' do
+        expect(user.upvoted_billboard?(billboard.id)).to eq false
+      end
+    end
+
+    context 'when user has voted' do
+      let!(:vote) do
+        FactoryBot.create(
+          :vote,
+          user_id: user.id,
+          billboard_id: billboard.id,
+          direction: direction
+        )
+      end
+
+      context 'when upvoted' do
+        let(:direction) { 'up'}
+
+        it 'returns false' do
+          expect(user.downvoted_billboard?(billboard.id)).to eq false
+        end
+      end
+
+      context 'when downvoted' do
+        let(:direction) { 'down'}
+
+        it 'returns true' do
+          expect(user.downvoted_billboard?(billboard.id)).to eq true
+        end
+      end
+    end
   end
 
   describe '#vote_results' do
